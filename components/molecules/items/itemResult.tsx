cf68ac2b0c486efbc153aef75fe9891714bcb426
minki <uuuu66@naver.com>
2021-09-23 01:30:43 +0900

    feat. result 결과창 수정 이제 부가효과의 아이템  레어도를 알 수 있음

--- components/molecules/items/itemResult.tsx
+++ components/molecules/items/itemResult.tsx
@@ -11,13 +11,13 @@ export interface Iabillities{
     "이동속도":number;
     "공격속도":number;
     "부가효과":object;
-   
 }
 const Result=({slot}:ComponentProps<any>)=>{
     
     const abillities=getSlotsAbillities(slot)
     const abilNames=Object.keys(abillities[0]); 
     const renderAbills= useCallback((value,i,name)=>{
+       
         return(
         <div key={i}>
             <Span rarity="유니크">{name}</Span>
@@ -30,32 +30,37 @@ const Result=({slot}:ComponentProps<any>)=>{
        
         const items=Object.keys(value[name]);
         return(
-            <div key={i}>
-                <Span rarity="유니크">{name}</Span>    
-                {items.map(item=>{
-                        return (<div className="sideEffect" key={item}>
-                            <br></br>
-                            <br></br>
-                             <Span> {item} </Span> 
-                             {value[name][item].map(effect=>{
-                                 return(
-                                     <p key={effect+item+i}>
-                                     <Span rarity="언커먼">{effect}</Span>
-                                     </p>
-                                 )
-                             })}
-                             </div>
-                            )        
-                    })
-                }      
-                    
-            </div>   
+            <div className="sideEffectWrap" key={i}>
+                <h1>
+                <Span rarity="유니크">{name}</Span>   
+                </h1>
+                <h2>요약</h2>
+                <div className="sideEffectSummary">
+                    {items.map(item=>{
+                            return (<div className="sideEffect" key={item}>
+                               <Span rarity={item.split("@?!")[1]}>{item.split("@?!")[0]}</Span>
+                                {value[name][item].map(effect=>{
+                                    return(
+                                        <p key={effect+item+i}>
+                                        <Span rarity="언커먼">{effect}</Span>
+                                        </p>
+                                    )
+                                })}
+                                </div>
+                                )        
+                        })
+                    } 
+                    </div>
+                
+                </div>     
         )
     },[slot])
+    
     return(<div className="result">
             <div className="abillity">
             {abillities.map((value,i)=>{
                         return <div key={i}>{abilNames.map((name,i)=>{
+                                 
                                   return name!="부가효과"?renderAbills(value,i,name):renderSideEffect(value,i,name);   
                                  }
                             )}</div>
@@ -78,6 +83,7 @@ export function getSlotsAbillities(slot:Islot)
             "이동속도":0,
             "공격속도":0,
             "부가효과":{},
+            
         };
         for (const item in items) {
         items[item]!=null?processSplitAbillityStr(items[item],result):null;
@@ -89,20 +95,20 @@ export function getSlotsAbillities(slot:Islot)
     function processSplitAbillityStr(item:CslotInfo,result:Iabillities){
         const info=item.info;
         const name=item.name;
-       
+        const rarity=item.rarity;
         const infoStrings=info.split("\n");
         infoStrings.map((value,index)=>{
-            result=processClassfyAbillity(value,index,result,infoStrings,name)         
+            result=processClassfyAbillity(value,index,result,infoStrings,name,rarity)         
         })      
      return result
     }
-    function processClassfyAbillity(value:string,index,result:Iabillities,infoStrings:string[],name:string){
+    function processClassfyAbillity(value:string,index,result:Iabillities,infoStrings:string[],name:string,rarity:string){
         if(value=="")
             return result;
         if(index>4)
             return result;
            
-        result=processSetAbility(value,index,result,infoStrings,name);
+        result=processSetAbility(value,index,result,infoStrings,name,rarity);
         return result;
     }
     function processPercentToNum(percent:string){
@@ -110,9 +116,9 @@ export function getSlotsAbillities(slot:Islot)
         const abilNum=Number(subPercent);
         return abilNum;
     }
-    function processSetAbility(value:string,index:number,result:Iabillities,infoStrings:string[],name:string){
+    function processSetAbility(value:string,index:number,result:Iabillities,infoStrings:string[],name:string,rarity:string){
         if(value.split(":").length<=1){
-           processSetSideEffect([value],name)
+           processSetSideEffect([value],name,rarity)
            return result;
         }
         const abilName=value.split(":")[0].split(" ")[0]
@@ -120,17 +126,17 @@ export function getSlotsAbillities(slot:Islot)
         const abilNum=processPercentToNum(abilStrs[1]);
       
         processClassifySideEffectType(index,infoStrings)?
-        processSetSideEffect(abilStrs,name)
+        processSetSideEffect(abilStrs,name,rarity)
         :
-        processSetEffect(abilName,abilNum,name,abilStrs)
+        processSetEffect(abilName,abilNum,name,abilStrs,rarity)
         return result;
     }
-    function processSetSideEffect(abilStrs:string[],name:string){
-        result.부가효과[name]==null?result.부가효과[name]=[abilStrs]:result.부가효과[name].push(abilStrs);
+    function processSetSideEffect(abilStrs:string[],name:string,rarity:string){
+        result.부가효과[name+`@?!${rarity}`]==null?result.부가효과[name+`@?!${rarity}`]=[abilStrs]:result.부가효과[name+`@?!${rarity}`].push(abilStrs);
     }
-    function processSetEffect(abilName:string,abilNum:number,name:string,abilStrs:string[]){
+    function processSetEffect(abilName:string,abilNum:number,name:string,abilStrs:string[],rarity){
         if(abilName!=="방어력")
-        result.hasOwnProperty(abilName)?result[abilName]+=abilNum:processSetSideEffect(abilStrs,name); 
+        result.hasOwnProperty(abilName)?result[abilName]+=abilNum:processSetSideEffect(abilStrs,name,rarity); 
         else{
          result[abilName]=(((1-result[abilName]/100)*abilNum/100)+result[abilName]/100)*100;
         }
