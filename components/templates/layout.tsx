40f39bb752126a50ff1969d53ffb24ad78414f15
minki <uuuu66@naver.com>
2021-12-07 01:40:11 +0900

    refactor.index 에서 make 로 이동하게 함

--- components/templates/layout.tsx
+++ components/templates/layout.tsx
@@ -3,25 +3,10 @@ import Nav from './nav';
 
 
 export default function Layout({children}:{children:ReactNode}){
-    const [menuState,setMenuState]=useState({index:0,text:"메뉴"});
-    const [navList,setNavList]=useState([{index:0,href:"/",title:"home"},{index:1,href:"/make",title:"아이템 세트 만들기 "}])
-    const menuStatus =(e)=>{
-      
-        if(menuState.index==0){
-           
-            setMenuState({index:1,text:"열림"});
-        }else{
-            
-            setMenuState({index:0,text:"메뉴"});
-        }
-        e.target.blur();
-    }
+  
+    
     return(
         <section>
-            <div id="menuBtn" onClick= {menuStatus} >
-                {menuState.text}
-            </div>
-            {!menuState.index?null:<Nav onLink={function(e){menuStatus(e)}} data={navList}></Nav>}
             <article>
                 {children}
             </article>       
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,11 @@
-
+import router from "next/router"
+import Router from "next/router"
+import { useEffect } from "react"
 
 export default function Home() {
+  useEffect(()=>{
+    router.push('/make');
+  },[])
   return (
     <>
       info 를 읽고 만드세요.
